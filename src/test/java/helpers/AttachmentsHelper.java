dead72d96a1993fd40e142ff4acacfddcf4ca757
dianamishanich <diana-mishanich@ya.ru>
2020-12-16 00:02:46 +0300

    Simple Google Test

--- src/test/java/helpers/AttachmentsHelper.java
+++ src/test/java/helpers/AttachmentsHelper.java
@@ -5,14 +5,12 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class AttachmentsHelper {
-
 
     @Attachment(value = "{attachName}", type = "text/plain")
     public static String attachAsText(String attachName, String message) {
@@ -39,6 +37,7 @@ public class AttachmentsHelper {
     public static String getVideoUrl() {
         return "https://" + System.getProperty("remote.browser.url") + "/video/" + getSessionId() + ".mp4";
     }
+
     public static String getSessionId(){
         return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
--- src/test/java/tests/SimpleGoogleTest.java
+++ src/test/java/tests/SimpleGoogleTest.java
@@ -1,13 +1,18 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import io.qameta.allure.Step;
-import io.qameta.allure.selenide.AllureSelenide;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+
+import static io.qameta.allure.Allure.step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
@@ -16,20 +21,27 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
-import static io.qameta.allure.Allure.step;
 
 public class SimpleGoogleTest {
 
+    final static String REMOTE_BROWSER_URL = "https://user1:1234@" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
+
+    final static String SEARCH_QUERY = "Selenide";
+    final static String SEARCH_RESULT = "selenide.org";
+
     @BeforeAll
     static void setup() {
+
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
+        Configuration.remote = REMOTE_BROWSER_URL;
         Configuration.startMaximized = true;
+
     }
 
     @AfterEach
@@ -41,46 +53,43 @@ public class SimpleGoogleTest {
         attachVideo();
 
         closeWebDriver();
+
     }
 
     @Test
     @DisplayName("Успешный поисковый запрос в Google")
     void successfulSelenideSearchTest() {
 
-        String searchQuery = "Selenide";
-        String searchResults = "selenide.org";
-
         step("Открываем google.com",() -> {
             open("https://www.google.com");
         });
 
-        step("Вводим в поисковую строку " + searchQuery,() -> {
-            $(byName("q")).setValue(searchQuery).pressEnter();
+        step("Вводим в поисковую строку " + SEARCH_QUERY,() -> {
+            $(byName("q")).setValue(SEARCH_QUERY).pressEnter();
         });
 
-        step("Результат поиска должен содержать " + searchResults,() -> {
-            $("html").shouldHave(text(searchResults));
+        step("Результат поиска должен содержать " + SEARCH_RESULT,() -> {
+            $("html").shouldHave(text(SEARCH_RESULT));
         });
+
     }
 
     @Test
     @DisplayName("Тест с ошибкой")
     void unsuccessfulSelenideSearchTest() {
 
-        String searchQuery = "Selenide";
-        String searchResults = "selenide.org";
-
         step("Открываем google.com",() -> {
             open("https://www.google.com");
         });
 
-        step("Вводим в поисковую строку " + searchQuery,() -> {
-            $(byName("q")).setValue(searchQuery).pressEnter();
+        step("Вводим в поисковую строку " + SEARCH_QUERY,() -> {
+            $(byName("q")).setValue(SEARCH_QUERY).pressEnter();
         });
 
-        step("Результат поиска должен содержать " + searchResults,() -> {
-            $("html").shouldNotHave(text(searchResults));
+        step("Результат поиска должен содержать " + SEARCH_RESULT,() -> {
+            $("html").shouldNotHave(text(SEARCH_RESULT));
         });
+
     }
 
 }
